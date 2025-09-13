066ae10a8405776735a7999c199498871df729c3
Saurabh Isane <114484966+saurabhisane@users.noreply.github.com>
2025-09-13 12:09:34 +0530

    First commit

---
+++ pattern.c
@@ -0,0 +1,14 @@
+int main() {
+    int rows = 5;
+
+    // Outer loop to print all rows
+    for (int i = 0; i < rows; i++) {
+
+        // Inner loop to print 
+        // the * in each row
+        for (int j = 0; j < rows - i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
